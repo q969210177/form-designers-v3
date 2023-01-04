ee07684ff793968adece4e81e9506f4732897d26
chenxi <969210177@qq.com>
2023-01-04 15:39:41 +0800

    vue3直接传入一个组件 渲染一个组件

--- src/views/ztxHome.vue
+++ src/views/ztxHome.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="ztxHome">
     <!-- <router-view /> -->
+    <aaa></aaa>
   </div>
 </template>
 <script lang="ts" setup>
@@ -9,6 +10,8 @@ interface Iprops {
   compoent: Component;
 }
 const props = defineProps<Iprops>();
+const aaa = ref(props.compoent);
+console.log(props);
 </script>
 <style lang="scss" scoped>
 @import "~@/assets/layout.scss";
